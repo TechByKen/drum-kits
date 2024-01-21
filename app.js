7c58d0098b031bd35ebb500c698f4a7c6ee08a50
TechByKen <diala.kenneth98@gmail.com>
2024-01-21 09:16:45 -0800

    Updated the script file

--- app.js
+++ app.js
@@ -6,5 +6,14 @@ kits.forEach((kit) => {
   const btn = document.createElement("button");
   btn.classList.add("btn");
   btn.innerText = kit;
+  btn.style.backgroundImage = "url(images/" + kit + ".png)";
   container.appendChild(btn);
+
+  const audio = document.createElement("audio");
+  audio.src = "sounds/" + kit + ".mp3";
+  container.appendChild(audio);
+
+  btn.addEventListener("click", () => {
+    audio.play();
+  });
 });
