c5969541d5902abac2a35dd651e5d012debbfef0
TechByKen <diala.kenneth98@gmail.com>
2024-01-21 03:34:20 -0800

    Updated the script file

---
+++ app.js
@@ -0,0 +1,9 @@
+const kits = ["crash", "kick", "snare", "tom"];
+
+const container = document.querySelector(".container");
+
+kits.forEach((kit) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  container.appendChild(btn);
+});
