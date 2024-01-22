2455d37272ba868846906a10757674b18a3f4747
TechByKen <diala.kenneth98@gmail.com>
2024-01-22 04:01:50 -0800

    Updated the script file

--- app.js
+++ app.js
@@ -16,4 +16,14 @@ kits.forEach((kit) => {
   btn.addEventListener("click", () => {
     audio.play();
   });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === kit.slice(0, 1)) {
+      audio.play();
+      btn.style.transform = "scale(.9)";
+      setTimeout(() => {
+        btn.style.transform = "scale(1)";
+      }, 100);
+    }
+  });
 });
